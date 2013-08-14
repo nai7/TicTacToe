040e345c4e2da259c509d950a9e5b09ee5d2e861
Whitaker Trebella <wtrebella@gmail.com>
2013-08-14 15:40:04 -0500

    got basic board built

--- Assets/Walls.cs
+++ Assets/Walls.cs
@@ -1,81 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Walls : MonoBehaviour {	
-	private GameObject[] outerWalls;
-	private GameObject[] innerWalls;
+public class Walls : MonoBehaviour {
+	public GameObject wallCube;
+	public float boardSize = 10;
+	public float boardHeight = 10;
 	
 	// Use this for initialization
 	void Start () {
-		outerWalls = new GameObject[4];
-		innerWalls = new GameObject[4];
+		GameObject tempCube;
 		
 		for (int i = 0; i < 4; i++) {
-			GameObject outerWall = (GameObject)Resources.Load("Wall");
-			Instantiate(outerWall);
-				
-			outerWall.transform.localScale = new Vector3(11, 10, 1);
-
+			
 			switch (i) {
 			case 0:
-				outerWall.transform.position = new Vector3(-5.5f, 0.5f, 0.5f);
-				outerWall.transform.rotation = Quaternion.Euler(0, 90, 0);
+				tempCube = (GameObject)Instantiate(wallCube, new Vector3(-boardSize / 2f - 0.5f, boardHeight / 2f, 0.5f), Quaternion.Euler(0, 90, 0));
+				tempCube.transform.localScale = new Vector3(boardSize + 1, boardHeight, 1);
+				
+				tempCube = (GameObject)Instantiate(wallCube, new Vector3(-(boardSize / 2f) / 3f, (boardHeight - 1) / 2f, 0), Quaternion.Euler(0, 90, 0));
+				tempCube.transform.localScale = new Vector3(boardSize, boardHeight - 1, 0.2f);
+				
 				break;
 				
 			case 1:
-				outerWall.transform.position = new Vector3(0.5f, 0.5f, 5.5f);
-				outerWall.transform.rotation = Quaternion.identity;
+				tempCube = (GameObject)Instantiate(wallCube, new Vector3(0.5f, boardHeight / 2f, boardSize / 2f + 0.5f), Quaternion.identity);
+				tempCube.transform.localScale = new Vector3(boardSize + 1, boardHeight, 1);
+				
+				tempCube = (GameObject)Instantiate(wallCube, new Vector3(0, (boardHeight - 1) / 2f, (boardSize / 2f) / 3f), Quaternion.identity);
+				tempCube.transform.localScale = new Vector3(boardSize, boardHeight - 1, 0.2f);
+				
 				break;
 				
 			case 2:
-				outerWall.transform.position = new Vector3(5.5f, 0.5f, -0.5f);
-				outerWall.transform.rotation = Quaternion.Euler(0, 90, 0);
+				tempCube = (GameObject)Instantiate(wallCube, new Vector3(boardSize / 2f + 0.5f, boardHeight / 2f, -0.5f), Quaternion.Euler(0, 90, 0));
+				tempCube.transform.localScale = new Vector3(boardSize + 1, boardHeight, 1);
+				
+				tempCube = (GameObject)Instantiate(wallCube, new Vector3((boardSize / 2f) / 3f, (boardHeight - 1) / 2f, 0), Quaternion.Euler(0, 90, 0));
+				tempCube.transform.localScale = new Vector3(boardSize, boardHeight - 1, 0.2f);
+				
 				break;
 				
 			case 3:
-				outerWall.transform.position = new Vector3(-0.5f, 0.5f, -5.5f);
-				outerWall.transform.rotation = Quaternion.identity;
+				tempCube = (GameObject)Instantiate(wallCube, new Vector3(-0.5f, boardHeight / 2f, -boardSize / 2f - 0.5f), Quaternion.identity);
+				tempCube.transform.localScale = new Vector3(boardSize + 1, boardHeight, 1);
+				
+				tempCube = (GameObject)Instantiate(wallCube, new Vector3(0, (boardHeight - 1) / 2f, -(boardSize / 2f) / 3f), Quaternion.identity);
+				tempCube.transform.localScale = new Vector3(boardSize, boardHeight - 1, 0.2f);
+				
 				break;
 			
 			default:
 				break;
 			}
-			
-			outerWalls[i] = outerWall;
-		}
-		
-		for (int i = 0; i < 4; i++) {
-			GameObject innerWall = (GameObject)Resources.Load("Wall");
-			Instantiate(innerWall);
-			
-			innerWall.transform.localScale = new Vector3(10, 9, 0.2f);
-			
-			switch (i) {
-			case 0:
-				innerWall.transform.position = new Vector3(-2.5f, 0.5f, 0);
-				innerWall.transform.rotation = Quaternion.Euler(0, 90, 0);
-				break;
-				
-			case 1:
-				innerWall.transform.position = new Vector3(0, 0.5f, 2.5f);
-				innerWall.transform.rotation = Quaternion.identity;
-				break;
-				
-			case 2:
-				innerWall.transform.position = new Vector3(2.5f, 0.5f, 0);
-				innerWall.transform.rotation = Quaternion.Euler(0, 90, 0);
-				break;
-				
-			case 3:
-				innerWall.transform.position = new Vector3(0, 0.5f, -2.5f);
-				innerWall.transform.rotation = Quaternion.identity;
-				break;
-			
-			default:
-				break;
-			}
-			
-			innerWalls[i] = innerWall;
 		}
 	}
 	
